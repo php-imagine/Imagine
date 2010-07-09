ea2fccbd0d28300e4a35b07f7b5ccb51b139604c
Jeremy Mikola <jmikola@theopenskyproject.com>
2010-07-10 04:55:55 +0800

    Added switch case breaks after resolving an upload error

--- lib/Imagine/UploadedImage.php
+++ lib/Imagine/UploadedImage.php
@@ -12,19 +12,24 @@ class UploadedImage extends StandardImage
                 case \UPLOAD_ERR_INI_SIZE:
                 case \UPLOAD_ERR_FORM_SIZE:
                     $message = 'The image exceeds maximum upload size';
+                    break;
                 case \UPLOAD_ERR_PARTIAL:
                     $message = 'The image was only partially uploaded';
+                    break;
                 case \UPLOAD_ERR_NO_FILE:
                     $message = 'The image wasn\'t uploaded';
+                    break;
                 case \UPLOAD_ERR_NO_TMP_DIR:
                     $message = 'No temp. directory found';
+                    break;
                 case \UPLOAD_ERR_CANT_WRITE:
                     $message = 'Could not save file to disk';
+                    break;
                 case \UPLOAD_ERR_EXTENSION:
                     $message = 'PHP extension stopper the upload';
+                    break;
                 default:
                     $message = 'Unknown error';
-                break;
             }
             throw new \RuntimeException($message);
         }
