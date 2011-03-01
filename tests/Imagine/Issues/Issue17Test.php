3791a0b87b4727ce7b9f722d3c9114ef8c335d46
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-03-01 11:24:04 -0500

    fixed issue-17

---
+++ tests/Imagine/Issues/Issue17Test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Imagine\Issues;
+
+use Imagine\ImageInterface;
+
+use Imagine\Box;
+
+use Imagine\Gd\Imagine;
+
+class Issue17Test extends \PHPUnit_Framework_TestCase
+{
+    public function testShouldResize()
+    {
+        $size    = new Box(100, 10);
+        $imagine = new Imagine();
+
+        $imagine->open('tests/Imagine/Fixtures/large.jpg')
+            ->thumbnail($size, ImageInterface::THUMBNAIL_OUTBOUND)
+            ->save('tests/Imagine/Fixtures/resized.jpg');
+
+        $this->assertTrue(file_exists('tests/Imagine/Fixtures/resized.jpg'));
+        $this->assertEquals(
+            $size,
+            $imagine->open('tests/Imagine/Fixtures/resized.jpg')->getSize()
+        );
+
+        unlink('tests/Imagine/Fixtures/resized.jpg');
+    }
+}
