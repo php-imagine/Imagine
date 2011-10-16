0f45edf6f3d76aaba6bfe03cbe73161bab913c0d
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-10-15 22:06:02 -0700

    exclude _build from sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ release = '0.2.1-alpha'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
