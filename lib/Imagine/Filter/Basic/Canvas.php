9747a43bc11afba3e1c40261c26336fb0e268d4e
Christer Edvartsen <cogo@starzinger.net>
2012-01-10 19:22:31 +0100

    Added Canvas filter and test case

---
+++ lib/Imagine/Filter/Basic/Canvas.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Imagine package.
+ *
+ * (c) Bulat Shakirzyanov <mallluhuct@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Imagine\Filter\Basic;
+
+use Imagine\Filter\FilterInterface;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\BoxInterface;
+use Imagine\Image\PointInterface;
+use Imagine\Image\Color;
+
+class Canvas implements FilterInterface
+{
+    /**
+     * @var Imagine\Image\BoxInterface
+     */
+    private $size;
+
+    /**
+     * @var Imagine\Image\PointInterface
+     */
+    private $placement;
+
+    /**
+     * @var Imagine\Image\Color
+     */
+    private $background;
+
+    /**
+     * Constructs Canvas filter with given width and height and the placement of the current image
+     * inside the new canvas
+     *
+     * @param Imagine\Image\BoxInterface $size
+     * @param Imagine\Image\PointInterface $placement
+     * @param Imagine\Image\Color $background
+     */
+    public function __construct(BoxInterface $size, PointInterface $placement = null, Color $background = null)
+    {
+        $this->size = $size;
+        $this->placement = $placement;
+        $this->background = $background;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Imagine\Filter\FilterInterface::apply()
+     */
+    public function apply(ImageInterface $image)
+    {
+        return $image->canvas($this->size, $this->placement, $this->background);
+    }
+}
---
+++ tests/Imagine/Filter/Basic/CanvasTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Imagine package.
+ *
+ * (c) Bulat Shakirzyanov <mallluhuct@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Imagine\Filter\Basic;
+
+use Imagine\Image\Box;
+use Imagine\Image\BoxInterface;
+use Imagine\Image\Point;
+use Imagine\Image\PointInterface;
+use Imagine\Image\Color;
+use Imagine\Filter\FilterTestCase;
+
+class CanvasTest extends FilterTestCase
+{
+    /**
+     * @covers Imagine\Filter\Basic\Canvas::apply
+     *
+     * @dataProvider getDataSet
+     *
+     * @param BoxInterface $size
+     * @param PointInterface $placement
+     * @param Color $background
+     */
+    public function testShouldCanvasImageAndReturnResult(BoxInterface $size, PointInterface $placement = null, Color $background = null)
+    {
+        $image = $this->getImage();
+
+        $image->expects($this->once())
+            ->method('canvas')
+            ->with($size, $placement, $background)
+            ->will($this->returnValue($image));
+
+        $command = new Canvas($size, $placement, $background);
+
+        $this->assertSame($image, $command->apply($image));
+    }
+
+    /**
+     * Data provider for testShouldCanvasImageAndReturnResult
+     *
+     * @return array
+     */
+    public function getDataSet()
+    {
+        return array(
+            array(new Box(50, 15), new Point(10, 10), new Color('fff')),
+            array(new Box(300, 25), new Point(15, 15)),
+            array(new Box(123, 23)),
+        );
+    }
+}
