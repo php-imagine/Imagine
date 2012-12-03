d2305888a603938d1b3be2d904352312d7fd7b67
Romain Neutron <imprec@gmail.com>
2012-12-03 16:59:33 +0100

    Fix tests

--- tests/Imagine/Issues/Issue67Test.php
+++ tests/Imagine/Issues/Issue67Test.php
@@ -9,7 +9,6 @@ use Imagine\Exception\RuntimeException;
 
 class Issue67Test extends \PHPUnit_Framework_TestCase
 {
-    
     private function getImagine()
     {
         try {
@@ -31,6 +30,6 @@ class Issue67Test extends \PHPUnit_Framework_TestCase
         $imagine = $this->getImagine();
 
         $imagine->open('tests/Imagine/Fixtures/large.jpg')
-            ->save($invalidPath . 'myfile.jpg');
+            ->save($invalidPath . '/myfile.jpg');
     }
 }
