1fa19f289c14d76fcde59e5a4405c5d9b96c10d7
rjkip <github@reinierkip.nl>
2013-01-04 16:11:40 +0100

    Adding layer replacement test.

--- tests/Imagine/Image/AbstractLayersTest.php
+++ tests/Imagine/Image/AbstractLayersTest.php
@@ -19,18 +19,54 @@ abstract class AbstractLayersTest extends \PHPUnit_Framework_TestCase
 {
     public function testMerge()
     {
+        $red = new Color("#FF0000");
+        $image = $this->getPolygonImage($red);
+
+        $image->layers()->merge();
+        $this->assertEquals((string) $red, (string) $image->getColorAt(new Point(5,5)));
+    }
+
+    public function testReplace()
+    {
+        $red = new Color("#FF0000");
+        $red_image = $this->getPolygonImage($red);
+
+        $blue = new Color("#0000FF");
+        $blue_image = $this->getPolygonImage($blue);
+
+        # Replace our first red image layer with the first blue image layer
+        $red_layers = $red_image->layers();
+
+        $blue_layers = $blue_image->layers();
+        $blue_layers->rewind();
+
+        $red_layers->replace(0, $blue_layers->current());
+
+        # Merge all layers and "save" to PNG
+        $red_image->layers()->merge();
+        $red_image_string = $red_image->get("png");
+
+        # Load in PNG
+        $red_image = $this->getImagine()->load($red_image_string);
+
+        # Assert the red image is now actually blue
+        $this->assertEquals((string) $blue, (string) $red_image->getColorAt(new Point(5,5)));
+    }
+
+    protected function getPolygonImage(Color $color)
+    {
         $image = $this->getImagine()->create(new Box(20, 20), new Color('#FFFFFF'));
+     
         foreach($image->layers() as $layer) {
             $layer->draw()
                 ->polygon(
                 array(new Point(0, 0),new Point(0, 20),new Point(20, 20),new Point(20, 0)),
-                new Color('#FF0000'),
+                $color,
                 true
             );
         }
-        $image->layers()->merge();
 
-        $this->assertEquals('#ff0000', (string) $image->getColorAt(new Point(5,5)));
+        return $image;
     }
 
     abstract protected function getImagine();
