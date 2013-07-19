1fb8831c237c7b0594e1d0924a758d67ab959f43
frickelbruder <mah@netvisio.de>
2013-07-19 19:49:25 +0200

    Moved tests to Testnamespace

---
+++ CorrectExifRotationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Imagine package.
+ *
+ * (c) Bulat Shakirzyanov <mallluhuct@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Imagine\Filter\Advanced;
+
+use Imagine\Image\Point;
+use Imagine\Image\Color;
+use Imagine\Filter\FilterTestCase;
+
+class CorrectExifRotationTest extends FilterTestCase
+{
+
+    public function testCorrectExifRotationWith90Deg()
+    {
+
+        $exif = array('Orientation' => 6);
+        $image       = $this->getImage();
+
+        $image->expects($this->once())
+              ->method('rotate')
+              ->with(90);
+
+        $filter->apply($image)
+
+
+
+        /*$color       = new Color('fff');
+        $width       = 2;
+        $height      = 4;
+        $image       = $this->getImage();
+        $imageWidth  = 200;
+        $imageHeight = 100;
+
+        $size = $this->getMock('Imagine\\Image\\BoxInterface');
+        $size->expects($this->once())
+             ->method('getWidth')
+             ->will($this->returnValue($width));
+
+        $size->expects($this->once())
+             ->method('getHeight')
+             ->will($this->returnValue($height));
+
+        $draw = $this->getDrawer();
+        $draw->expects($this->exactly(4))
+             ->method('line')
+             ->will($this->returnValue($draw));
+
+        $image->expects($this->once())
+              ->method('getSize')
+              ->will($this->returnValue($size));
+
+        $image->expects($this->once())
+              ->method('draw')
+              ->will($this->returnValue($draw));
+
+        $filter = new Border($color, $width, $height);
+
+        $this->assertSame($image, $filter->apply($image));*/
+    }
+}
