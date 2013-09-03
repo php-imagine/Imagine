218950096a18a67a5e03781abf358fef1dc2ddfb
Tony Lemke <tlemke@naji-dev.de>
2013-09-03 08:30:27 +0200

    changed NegationFilter to use EffectsInterface::negative()

--- tests/Imagine/Test/Filter/Advanced/NegationTest.php
+++ tests/Imagine/Test/Filter/Advanced/NegationTest.php
@@ -12,74 +12,29 @@
 namespace Imagine\Test\Filter\Advanced;
 
 use Imagine\Filter\Advanced\Negation;
-use Imagine\Image\Palette\RGB;
 use Imagine\Image\Point;
 use Imagine\Test\Filter\FilterTestCase;
 
-
-/**
- * @author Tony Lemke <naji@mail.upb.de>
- */
 class NegationTest extends FilterTestCase
 {
-    /**
-     * @dataProvider getData
-     */
-    public function testCallback($currentColor, $expectedColor)
+    public function testApplyingNegation()
     {
-        $rgb = new RGB();
-        $image = $this->getImage();
-
-        $size = $this->getMock('Imagine\\Image\\BoxInterface');
-        $size
-            ->expects($this->any())
-            ->method('getWidth')
-            ->will($this->returnValue(1))
-        ;
-
-        $size
-            ->expects($this->any())
-            ->method('getHeight')
-            ->will($this->returnValue(1))
-        ;
-
-        $image
-            ->expects($this->any())
-            ->method('getSize')
-            ->will($this->returnValue($size))
-        ;
-
-        $currentColor = $rgb->color($currentColor);
-
-        $image
-            ->expects($this->any())
-            ->method('getColorAt')
-            ->will($this->returnValue($currentColor))
-        ;
-
-        $drawer = $this->getDrawer();
-        $drawer
+        $effects = $this->getMock('Imagine\Effects\EffectsInterface');
+        $effects
             ->expects($this->once())
-            ->method('dot')
-            ->with(new Point(0, 0), $rgb->color($expectedColor))
+            ->method('negative')
+            ->will($this->returnValue($effects))
         ;
 
+        $image = $this->getImage();
         $image
-            ->expects($this->any())
-            ->method('draw')
-            ->will($this->returnValue($drawer))
+            ->expects($this->once())
+            ->method('effects')
+            ->will($this->returnValue($effects))
         ;
 
-        $blackWhiteFilter = new Negation();
-        $blackWhiteFilter->apply($image);
-    }
+        $filter = new Negation();
 
-    public function getData()
-    {
-        return array(
-            array(array(150, 124, 83), array(105, 131, 172)),
-            array(array(132, 169, 31), array(123, 86, 224)),
-            array(array(234, 122, 11), array(21, 133, 244))
-        );
+        $this->assertEquals($image, $filter->apply($image));
     }
 }
