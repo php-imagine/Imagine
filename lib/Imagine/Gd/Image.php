d563aaad184ba1fe3849daca5c3c34dfe5ab8bcc
Alessio <io.alessio@gmail.com>
2013-09-18 15:12:54 +0200

    Update Image.php

--- lib/Imagine/Gd/Image.php
+++ lib/Imagine/Gd/Image.php
@@ -124,12 +124,12 @@ final class Image implements ImageInterface
         }
 
         $size = $image->getSize();
-        if (!$this->getSize()->contains($size, $start)) {
-            throw new OutOfBoundsException(
-                'Cannot paste image of the given size at the specified '.
-                'position, as it moves outside of the current image\'s box'
-            );
-        }
+        //if (!$this->getSize()->contains($size, $start)) {
+        //    throw new OutOfBoundsException(
+        //        'Cannot paste image of the given size at the specified '.
+        //        'position, as it moves outside of the current image\'s box'
+        //    );
+        //}
 
         imagealphablending($this->resource, true);
         imagealphablending($image->resource, true);
