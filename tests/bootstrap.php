4c8caeb12d03d5e00c289833681fc4e474ce5a22
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2014-06-04 14:26:35 +0200

    updated PHPUnit dev requirement and increased memory limit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,8 +9,8 @@
  * file that was distributed with this source code.
  */
 
-if (intval(ini_get('memory_limit')) < 32) {
-    ini_set('memory_limit', '32M');
+if (intval(ini_get('memory_limit')) < 64) {
+    ini_set('memory_limit', '64M');
 }
 
 $loader = require __DIR__.'/../vendor/autoload.php';
