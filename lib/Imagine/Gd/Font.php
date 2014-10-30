becfa43062c1b500d52eafb193cd6b6640568802
Vladimir Leitenberger <django.websitemailer@gmail.com>
2014-10-30 17:28:56 +0100

    Fix for Issue #364. — added Font::wrapText() method which is a reflection of Drawer::wrapText()

--- lib/Imagine/Gd/Font.php
+++ lib/Imagine/Gd/Font.php
@@ -23,12 +23,20 @@ final class Font extends AbstractFont
     /**
      * {@inheritdoc}
      */
-    public function box($string, $angle = 0)
+    public function box($string, $angle = 0, $width = null)
     {
         if (!function_exists('imageftbbox')) {
             throw new RuntimeException('GD must have been compiled with `--with-freetype-dir` option to use the Font feature.');
         }
 
+				/**
+         * @see https://github.com/avalanche123/Imagine/issues/364
+				 * Add a wrapText() method from Drawer using Reflection-API
+         */
+				if ($width !== null) {
+            $string = $this->wrapText($string, $this, $angle, $width);
+        }
+				
         $angle    = -1 * $angle;
         $info     = imageftbbox($this->size, $angle, $this->file, $string);
         $xs       = array($info[0], $info[2], $info[4], $info[6]);
@@ -37,5 +45,29 @@ final class Font extends AbstractFont
         $height   = abs(max($ys) - min($ys));
 
         return new Box($width, $height);
+    }
+		
+		/**
+     * Internal
+     *
+     * Fits a string into box with given width
+     */
+    private function wrapText($string, AbstractFont $font, $angle, $width)
+    {
+        //Use Reflection
+				$class_name = '\\Drawer'; //escaped slash
+				$ref_class = new \ReflectionClass( __NAMESPACE__ . $class_name );
+				if ( !$ref_class->hasMethod(__FUNCTION__) )
+				{
+						throw new \ReflectionException(sprintf("Method %s::%s does not exist!", $ref_class->getName() , __FUNCTION__));
+				}
+				$new_ref_obj = $ref_class->newInstanceWithoutConstructor();
+				$ref_method_to_call = new \ReflectionMethod($new_ref_obj, __FUNCTION__);
+				if( $ref_method_to_call->isPrivate() )
+				{
+						$ref_method_to_call->setAccessible(true);
+				}
+
+				return $ref_method_to_call->invoke($new_ref_obj, $string, $font, $angle, $width);
     }
 }
