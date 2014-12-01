164459378931d54f1efbbcff22688e13a6d36dcf
vlakoff <vlakoff@gmail.com>
2014-12-01 06:38:00 +0100

    Add THUMBNAIL_UPSCALE setting
    This setting allows upscaling the source image if needed,
    so that the result matches the requested size instead of being smaller.

--- lib/Imagine/Image/AbstractImage.php
+++ lib/Imagine/Image/AbstractImage.php
@@ -41,6 +41,8 @@ abstract class AbstractImage implements ImageInterface
             throw new InvalidArgumentException('Invalid mode specified');
         }
 
+        $allowUpscale = (bool) ($settings & ImageInterface::THUMBNAIL_UPSCALE);
+
         $imageSize = $this->getSize();
         $ratios = array(
             $size->getWidth() / $imageSize->getWidth(),
@@ -53,7 +55,7 @@ abstract class AbstractImage implements ImageInterface
         $thumbnail->strip();
         // if target width is larger than image width
         // AND target height is longer than image height
-        if ($size->contains($imageSize)) {
+        if ($size->contains($imageSize) && !$allowUpscale) {
             return $thumbnail;
         }
 
@@ -68,6 +70,10 @@ abstract class AbstractImage implements ImageInterface
             $thumbnail->resize($imageSize, $filter);
         } else {
             if (!$imageSize->contains($size)) {
+                if ($allowUpscale) {
+                    $imageSize = $imageSize->scale($ratio);
+                    $thumbnail->resize($imageSize, $filter);
+                }
                 $size = new Box(
                     min($imageSize->getWidth(), $size->getWidth()),
                     min($imageSize->getHeight(), $size->getHeight())
