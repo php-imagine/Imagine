834978689f5c53374468c733e4fa20efe2a0c423
Amri Sannang <amri.sannang@alaress.com.au>
2015-05-27 20:59:36 +1000

    Added test case for animated GIF crop

--- tests/Imagine/Test/Imagick/ImageTest.php
+++ tests/Imagine/Test/Imagick/ImageTest.php
@@ -13,6 +13,7 @@ namespace Imagine\Test\Imagick;
 
 use Imagine\Image\ImageInterface;
 use Imagine\Image\Metadata\MetadataBag;
+use Imagine\Image\Point;
 use Imagine\Imagick\Imagine;
 use Imagine\Imagick\Image;
 use Imagine\Image\Palette\CMYK;
@@ -91,6 +92,24 @@ class ImageTest extends AbstractImageTest
     public function testOlderImageMagickDoesNotAffectColorspaceUsageOnPaletteChange($image)
     {
         $image->usePalette(new RGB());
+    }
+
+    public function testAnimatedGifCrop()
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->open('tests/Imagine/Fixtures/anima3.gif');
+        $image
+            ->crop(
+                new Point(0, 0),
+                new Box(150, 100)
+            )
+            ->save('tests/Imagine/Fixtures/crop/anima3-topleft-actual.gif', array('animated' => true))
+        ;
+        $this->assertImageEquals(
+            $imagine->open('tests/Imagine/Fixtures/crop/anima3-topleft.gif'),
+            $imagine->open('tests/Imagine/Fixtures/crop/anima3-topleft-actual.gif')
+        );
+        unlink('tests/Imagine/Fixtures/crop/anima3-topleft-actual.gif');
     }
 
     protected function supportMultipleLayers()
