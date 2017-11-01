8c0a7822a8322ac4c9501cede748988b1cb3f0a6
goncons <goncons@users.noreply.github.com>
2017-11-01 13:02:04 -0400

    Minor changes/corrections.
    - Methods circle and rectangle added.
    - Set stroke and fill opacity ('transparent' ? 0 : 1) when stroke an fill color is set.

--- lib/Imagine/Gmagick/Drawer.php
+++ lib/Imagine/Gmagick/Drawer.php
@@ -44,6 +44,11 @@ final class Drawer implements DrawerInterface
      */
     public function arc(PointInterface $center, BoxInterface $size, $start, $end, ColorInterface $color, $thickness = 1)
     {
+    	if (0 === ($thickness = max(1, (int) $thickness))) {
+    		// If $thickness is 0 do nothing (the arc is not going to be drawn):
+    		return $this;
+    	}
+
         $x      = $center->getX();
         $y      = $center->getY();
         $width  = $size->getWidth();
@@ -52,10 +57,12 @@ final class Drawer implements DrawerInterface
         try {
             $pixel = $this->getColor($color);
             $arc   = new \GmagickDraw();
+            
+            $arc->setstrokewidth($thickness);
+            $this->setStrokeColorOpacity($arc, $pixel)
+            
+            	 ->setFillColorOpacity($arc, 'transparent');
 
-            $arc->setstrokecolor($pixel);
-            $arc->setstrokewidth(max(1, (int) $thickness));
-            $arc->setfillcolor('transparent');
             $arc->arc(
                 $x - $width / 2,
                 $y - $height / 2,
@@ -82,6 +89,11 @@ final class Drawer implements DrawerInterface
      */
     public function chord(PointInterface $center, BoxInterface $size, $start, $end, ColorInterface $color, $fill = false, $thickness = 1)
     {
+    	if ((0 === ($thickness = max(1, (int) $thickness))) && (!$fill)) {
+    		// If $thickness is 0 and the cord is not filled, do nothing (the chord is not going to be drawn):
+    		return $this;
+    	}
+
         $x      = $center->getX();
         $y      = $center->getY();
         $width  = $size->getWidth();
@@ -91,23 +103,31 @@ final class Drawer implements DrawerInterface
             $pixel = $this->getColor($color);
             $chord = new \GmagickDraw();
 
-            $chord->setstrokecolor($pixel);
-            $chord->setstrokewidth(max(1, (int) $thickness));
+            $chord->setstrokewidth($thickness);
+            $this->setStrokeColorOpacity($chord, 0 === $thickness ? 'transparent' : $pixel);
 
             if ($fill) {
-                $chord->setfillcolor($pixel);
+            	$this->setFillColorOpacity($chord, $pixel);
             } else {
-                $x1 = round($x + $width / 2 * cos(deg2rad($start)));
-                $y1 = round($y + $height / 2 * sin(deg2rad($start)));
-                $x2 = round($x + $width / 2 * cos(deg2rad($end)));
-                $y2 = round($y + $height / 2 * sin(deg2rad($end)));
+            	$start = deg2rad($start);
+            	$end   = deg2rad($end);
+            	$this->line(
+            		new Point(round($x + $width / 2 * cos($start)), round($y + $height / 2 * sin($start))),
+            		new Point(round($x + $width / 2 * cos($end)), round($y + $height / 2 * sin($end))),
+            		$color
+            	);
 
-                $this->line(new Point($x1, $y1), new Point($x2, $y2), $color);
-
-                $chord->setfillcolor('transparent');
+                $this->setFillColorOpacity($chord, 'transparent');
             }
 
-            $chord->arc($x - $width / 2, $y - $height / 2, $x + $width / 2, $y + $height / 2, $start, $end);
+            $chord->arc(
+           		$x - $width / 2,
+            	$y - $height / 2,
+            	$x + $width / 2,
+            	$y + $height / 2,
+            	$start,
+            	$end
+            );
 
             $this->gmagick->drawImage($chord);
 
@@ -120,12 +140,26 @@ final class Drawer implements DrawerInterface
 
         return $this;
     }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function circle(PointInterface $center, $radius, ColorInterface $color, $fill = false, $thickness = 1)
+    {
+    	// Gmagick do not have a circle function, ellipse is used:
+    	return $this->ellipse($center, new Box($radius, $radius), $color, $fill, $thickness);
+    }
 
     /**
      * {@inheritdoc}
      */
     public function ellipse(PointInterface $center, BoxInterface $size, ColorInterface $color, $fill = false, $thickness = 1)
     {
+    	if ((0 === ($thickness = max(1, (int) $thickness))) && (!$fill)) {
+    		// If $thickness is 0 and the ellipse is not filled, do nothing (the ellipse is not going to be drawn):
+    		return $this;
+    	}
+
         $width  = $size->getWidth();
         $height = $size->getHeight();
 
@@ -133,14 +167,10 @@ final class Drawer implements DrawerInterface
             $pixel   = $this->getColor($color);
             $ellipse = new \GmagickDraw();
 
-            $ellipse->setstrokecolor($pixel);
-            $ellipse->setstrokewidth(max(1, (int) $thickness));
+            $ellipse->setstrokewidth($thickness);
+            $this->setStrokeColorOpacity($ellipse, 0 === $thickness ? 'transparent' : $pixel)
 
-            if ($fill) {
-                $ellipse->setfillcolor($pixel);
-            } else {
-                $ellipse->setfillcolor('transparent');
-            }
+                 ->setFillColorOpacity($ellipse, $fill ? $pixel : 'transparent');
 
             $ellipse->ellipse(
                 $center->getX(),
@@ -167,13 +197,20 @@ final class Drawer implements DrawerInterface
      */
     public function line(PointInterface $start, PointInterface $end, ColorInterface $color, $thickness = 1)
     {
+    	if (0 === ($thickness = max(1, (int) $thickness))) {
+    		// If $thickness is 0 do nothing (the line is not going to be drawn):
+    		return $this;
+    	}
+
         try {
             $pixel = $this->getColor($color);
             $line  = new \GmagickDraw();
 
-            $line->setstrokecolor($pixel);
-            $line->setstrokewidth(max(1, (int) $thickness));
-            $line->setfillcolor($pixel);
+            $line->setstrokewidth($thickness);
+            $this->setStrokeColorOpacity($line, $pixel)
+
+                 ->setFillColorOpacity($line, $pixel);
+
             $line->line(
                 $start->getX(),
                 $start->getY(),
@@ -198,6 +235,11 @@ final class Drawer implements DrawerInterface
      */
     public function pieSlice(PointInterface $center, BoxInterface $size, $start, $end, ColorInterface $color, $fill = false, $thickness = 1)
     {
+    	if ((0 === ($thickness = max(1, (int) $thickness))) && (!$fill)) {
+    		// If $thickness is 0 and the pieSlice is not filled, do nothing (the pieSlice is not going to be drawn):
+    		return $this;
+    	}
+
         $width  = $size->getWidth();
         $height = $size->getHeight();
 
@@ -239,12 +281,14 @@ final class Drawer implements DrawerInterface
             $pixel = $this->getColor($color);
             $point = new \GmagickDraw();
 
-            $point->setfillcolor($pixel);
+            $this->setFillColorOpacity($point, $pixel);
+
             $point->point($x, $y);
 
             $this->gmagick->drawimage($point);
 
             $pixel = null;
+
             $point = null;
         } catch (\GmagickException $e) {
             throw new RuntimeException('Draw point operation failed', $e->getCode(), $e);
@@ -258,6 +302,11 @@ final class Drawer implements DrawerInterface
      */
     public function polygon(array $coordinates, ColorInterface $color, $fill = false, $thickness = 1)
     {
+    	if ((0 === ($thickness = max(1, (int) $thickness))) && (!$fill)) {
+    		// If $thickness is 0 and the polygon is not filled, do nothing (the polygon is not going to be drawn):
+    		return $this;
+    	}
+
         if (count($coordinates) < 3) {
             throw new InvalidArgumentException(sprintf('Polygon must consist of at least 3 coordinates, %d given', count($coordinates)));
         }
@@ -270,25 +319,61 @@ final class Drawer implements DrawerInterface
             $pixel   = $this->getColor($color);
             $polygon = new \GmagickDraw();
 
-            $polygon->setstrokecolor($pixel);
-            $polygon->setstrokewidth(max(1, (int) $thickness));
+            $polygon->setstrokewidth($thickness);
+            $this->setStrokeColorOpacity($polygon, 0 === $thickness ? 'transparent' : $pixel)
 
-            if ($fill) {
-                $polygon->setfillcolor($pixel);
-            } else {
-                $polygon->setfillcolor('transparent');
-            }
+                 ->setFillColorOpacity($polygon, $fill ? $pixel : 'transparent');
 
             $polygon->polygon($points);
 
             $this->gmagick->drawImage($polygon);
 
-            unset($pixel, $polygon);
+            $pixel   = null;
+            
+            $polygon = null;
         } catch (\GmagickException $e) {
             throw new RuntimeException('Draw polygon operation failed', $e->getCode(), $e);
         }
 
         return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rectangle(PointInterface $leftTop, PointInterface $rightBottom, ColorInterface $color, $fill = false, $thickness = 1)
+    {
+    	if ((0 === ($thickness = max(1, (int) $thickness))) && (!$fill)) {
+    		// If $thickness is 0 and the polygon is not filled, do nothing (the polygon is not going to be drawn):
+    		return $this;
+    	}
+    	
+    	try {
+    		$pixel     = $this->getColor($color);
+    		$rectangle = new \GmagickDraw();
+    		
+    		$rectangle->setstrokewidth($thickness);
+    		$this->setStrokeColorOpacity($rectangle, 0 === $thickness ? 'transparent' : $pixel)
+    		
+    		     ->setFillColorOpacity($rectangle, $fill ? $pixel : 'transparent');
+    		
+    		$rectangle->rectangle(
+    			$leftTop->getX(),
+    			$leftTop->getY(),
+    			$rightBottom->getX(),
+    			$rightBottom->getY()
+    		);
+    		
+    		$this->gmagick->drawImage($rectangle);
+    		
+    		$pixel     = null;
+    		
+    		$rectangle = null;
+    	} catch (\ImagickException $e) {
+    		throw new RuntimeException('Draw rectangle operation failed', $e->getCode(), $e);
+    	}
+    	
+    	return $this;
     }
 
     /**
@@ -307,7 +392,7 @@ final class Drawer implements DrawerInterface
              * ensure font resolution is the same as GD's hard-coded 96
              */
             $text->setfontsize((int) ($font->getSize() * (96 / 72)));
-            $text->setfillcolor($pixel);
+            $this->setFillColorOpacity($text, $pixel);
 
             $info = $this->gmagick->queryfontmetrics($text, $string);
             $rad  = deg2rad($angle);
@@ -339,7 +424,7 @@ final class Drawer implements DrawerInterface
     /**
      * Gets specifically formatted color string from Color instance
      *
-     * @param ColorInterface $color
+     * @param  ColorInterface $color
      *
      * @return \GmagickPixel
      *
@@ -352,5 +437,39 @@ final class Drawer implements DrawerInterface
         }
 
         return new \GmagickPixel((string) $color);
+    }
+    
+    /**
+     * Set color and opacity (0 if === 'transparent', 1 if !== 'transparent') of a fill
+     *
+     * @param  \GmagickDraw         $draw
+     * @param  string|\GmagickPixel $pixel
+     *
+     * @return DrawerInterface
+     */
+    private function setFillColorOpacity(\GmagickDraw $draw, $pixel)
+    {
+    	$draw->setfillcolor($pixel);
+    	// Ensure that the stroke is visible:
+    	$draw->setfillopacity('transparent' === $pixel ? 0 : 1);
+    	
+    	return $this;
+    }
+    
+    /**
+     * Set color and opacity (0 if === 'transparent', 1 if !== 'transparent') of a stroke
+     *
+     * @param  \GmagickDraw         $draw
+     * @param  string|\GmagickPixel $pixel
+     *
+     * @return DrawerInterface
+     */
+    private function setStrokeColorOpacity(\GmagickDraw $draw, $pixel)
+    {
+    	$draw->setstrokecolor($pixel);
+    	// Ensure that the stroke is visible:
+    	$draw->setstrokeopacity('transparent' === $pixel ? 0 : 1);
+    	
+    	return $this;
     }
 }
