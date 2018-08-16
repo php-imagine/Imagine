1a8695c4d0b27d70f91e596f1bb8491ff2d1bd36
Michele Locati <michele@locati.it>
2018-08-16 15:18:53 +0200

    Fix URL of remote test images

--- tests/Imagine/Test/ImagineTestCase.php
+++ tests/Imagine/Test/ImagineTestCase.php
@@ -15,7 +15,7 @@ use Imagine\Test\Constraint\IsImageEqual;
 
 class ImagineTestCase extends \PHPUnit\Framework\TestCase
 {
-    const HTTP_IMAGE = 'http://imagine.readthedocs.org/en/latest/_static/logo.jpg';
+    const HTTP_IMAGE = 'https://imagine.readthedocs.org/en/latest/_static/logo.jpg';
 
     /**
      * Asserts that two images are equal using color histogram comparison method
