1ce09988d93c2b7589c73f46e3e18a692f2a15e2
Michele Locati <michele@locati.it>
2018-08-18 15:19:36 +0200

    Fix imagefttext() and imageftbbox() calls on Windows

--- lib/Imagine/Gd/Font.php
+++ lib/Imagine/Gd/Font.php
@@ -28,9 +28,17 @@ final class Font extends AbstractFont
         if (!function_exists('imageftbbox')) {
             throw new RuntimeException('GD must have been compiled with `--with-freetype-dir` option to use the Font feature.');
         }
+        $fontfile = $this->file;
+        if ($fontfile && DIRECTORY_SEPARATOR === '\\') {
+            // On Windows imageftbbox() throws a "Could not find/open font" error if $fontfile is not an absolute path.
+            $fontfileRealpath = realpath($fontfile);
+            if ($fontfileRealpath !== false) {
+                $fontfile = $fontfileRealpath;
+            }
+        }
 
         $angle    = -1 * $angle;
-        $info     = imageftbbox($this->size, $angle, $this->file, $string);
+        $info     = imageftbbox($this->size, $angle, $fontfile, $string);
         $xs       = array($info[0], $info[2], $info[4], $info[6]);
         $ys       = array($info[1], $info[3], $info[5], $info[7]);
         $width    = abs(max($xs) - min($xs));
