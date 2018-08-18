4a87f8b9b18d0fba7f75509563807ac73265e1e5
Michele Locati <michele@locati.it>
2018-08-18 14:20:26 +0200

    Enable EXIF extension

--- .appveyor/configure.ps1
+++ .appveyor/configure.ps1
@@ -47,7 +47,7 @@ if (Test-Path -PathType Leaf -Path "$phpInstallPath\php-installed.txt") {
 	Install-Php -Version $Env:PHP_VERSION -Architecture $Env:PHP_ARCHITECTURE -ThreadSafe $false -Path $phpInstallPath -TimeZone UTC -InitialPhpIni Production -InstallVC -Force -Verbose
 	Set-PhpIniKey -Path $phpInstallPath -Key zend.assertions -Value 1
 	Set-PhpIniKey -Path $phpInstallPath -Key assert.exception -Value On
-	Enable-PhpExtension -Path $phpInstallPath -Extension mbstring,curl,openssl,gd,zlib -Verbose
+	Enable-PhpExtension -Path $phpInstallPath -Extension mbstring,curl,openssl,gd,exif,zlib -Verbose
 	Install-PhpExtension -Path $phpInstallPath -Extension imagick -Verbose
 	New-Item -ItemType File -Path "$phpInstallPath\php-installed.txt" | Out-Null
 }
