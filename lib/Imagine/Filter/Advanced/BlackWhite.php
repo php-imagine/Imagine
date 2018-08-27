270203094a764c40ef9efc982499eb843f71eb48
Michele Locati <michele@locati.it>
2018-08-27 18:33:07 +0200

    Fix phpdoc of BlackWhite

--- lib/Imagine/Filter/Advanced/BlackWhite.php
+++ lib/Imagine/Filter/Advanced/BlackWhite.php
@@ -19,15 +19,21 @@ use Imagine\Image\Palette\RGB;
 use Imagine\Image\Point;
 
 /**
- * This filter calculates for each pixel of an image, whether it is more white or black and so makes it white or black.
- * Therefore each pixel is grayscaled and and the resulting value is compared with $threshold. Is the value smaller than
- * the threshold, the pixel will be white, Otherwise it will be black. Result is an image with only black and white
- * pixels.
+ * This filter calculates, for each pixel of an image, whether it is ligher or darker than a threshold.
+ * If the pixel is lighter than the thresold it will be black, otherwise it will be light.
+ * The result is an image with only black and white pixels (black pixels for ligher colors, white pixels for darker colors).
  */
 class BlackWhite extends OnPixelBased implements FilterInterface
 {
     protected $grayScaleFilter;
 
+    /**
+     * Initialize this filter.
+     *
+     * @param int $threshold the dask/light threshold, from 0 (all black) to 255 (all white)
+     *
+     * @throws InvalidArgumentException
+     */
     public function __construct($threshold)
     {
         if (!(0 <= $threshold && $threshold <= 255)) {
@@ -39,11 +45,15 @@ class BlackWhite extends OnPixelBased implements FilterInterface
         $rgb = new RGB();
         parent::__construct(function (ImageInterface $image, Point $point) use ($threshold, $rgb) {
             $newRedValue = $image->getColorAt($point)->getValue(ColorInterface::COLOR_RED) < $threshold ? 255 : 0;
-
             $image->draw()->dot($point, $rgb->color(array($newRedValue, $newRedValue, $newRedValue)));
         });
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Imagine\Filter\Advanced\OnPixelBased::apply()
+     */
     public function apply(ImageInterface $image)
     {
         $grayScaledImage = $this->grayScaleFilter->apply($image);
