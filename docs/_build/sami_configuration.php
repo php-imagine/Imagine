54cf1c16661feab8008b7a2c22adf03d5fe38702
Michele Locati <michele@locati.it>
2018-09-12 10:13:06 +0200

    Add Imagine version to API docs

--- docs/_build/sami_configuration.php
+++ docs/_build/sami_configuration.php
@@ -3,14 +3,22 @@
 use Sami\Sami;
 use Symfony\Component\Finder\Finder;
 
+$title = 'Imagine API';
+$srcDir = dirname(dirname(__DIR__)) . '/src';
+
+$imagineInterfaceSource = file_get_contents("{$srcDir}/Image/ImagineInterface.php");
+if (preg_match('/^\s*const\s+VERSION\s*=\s*(["\'])(.*)\\1\s*;/m', $imagineInterfaceSource, $m)) {
+    $title .= ' v' . ltrim($m[2], 'v. ');
+}
+
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in(dirname(dirname(__DIR__)) . '/src')
+    ->in($srcDir)
 ;
 
 return new Sami($iterator, array(
-    'title' => 'Imagine API',
+    'title' => $title,
     'theme' => 'default',
     'build_dir' => __DIR__ . '/../API/API',
     'cache_dir' => __DIR__ . '/cache',
