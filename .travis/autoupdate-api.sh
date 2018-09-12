d2310f0d4602f82ef4e88f01e78bd75fec0cef41
Michele Locati <michele@locati.it>
2018-09-12 14:11:24 +0200

    Always create a fresh API build

--- .travis/autoupdate-api.sh
+++ .travis/autoupdate-api.sh
@@ -52,6 +52,7 @@ echo "Automatic API generation: updating API"
 cd "${TRAVIS_BUILD_DIR}"
 git checkout -qf "${AUTO_PROCESS_BRANCH}"
 cd "${TRAVIS_BUILD_DIR}/docs/_build"
+rm -rf ../API
 composer --no-interaction install
 composer --no-interaction update-docs
 
