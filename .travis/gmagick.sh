aaaf30fcef3311464ce7a89e3b4400150fb8c71e
Michele Locati <michele@locati.it>
2018-10-24 14:01:24 +0200

    (Try to) fix travis builds

--- .travis/gmagick.sh
+++ .travis/gmagick.sh
@@ -2,20 +2,19 @@
 
 set -xe
 
-GRAPHICSMAGIC_VERSION="1.3.23"
-if [ ${TRAVIS_PHP_VERSION:0:1} = '7' ] || [ $TRAVIS_PHP_VERSION = 'nightly' ]
-then
-  GMAGICK_VERSION="2.0.4RC1"
+GRAPHICSMAGIC_VERSION='1.3.23'
+if [ ${TRAVIS_PHP_VERSION:0:1} = '5' ]; then
+  GMAGICK_VERSION='1.1.7RC2'
 else
-  GMAGICK_VERSION="1.1.7RC2"
+  GMAGICK_VERSION='2.0.4RC1'
 fi
+
 PHP_VERSION=`php -r 'echo PHP_VERSION_ID;'`
 
 mkdir -p cache
 cd cache
 
-if [ ! -e ./GraphicsMagick-$GRAPHICSMAGIC_VERSION ]
-then
+if [ ! -e ./GraphicsMagick-$GRAPHICSMAGIC_VERSION ]; then
     rm -rf ./GraphicsMagick-* || true
     wget http://78.108.103.11/MIRROR/ftp/GraphicsMagick/1.3/GraphicsMagick-$GRAPHICSMAGIC_VERSION.tar.xz
     tar -xf GraphicsMagick-$GRAPHICSMAGIC_VERSION.tar.xz
@@ -30,21 +29,20 @@ fi
 sudo make install
 cd ..
 
-if [ ! -e ./gmagick-$GMAGICK_VERSION-$PHP_VERSION ]
-then
+if [ ! -e ./gmagick-$GMAGICK_VERSION-$PHP_VERSION-$GRAPHICSMAGIC_VERSION ]; then
     rm -rf ./gmagick-* || true
     wget https://pecl.php.net/get/gmagick-$GMAGICK_VERSION.tgz
     tar -xzf gmagick-$GMAGICK_VERSION.tgz
     rm gmagick-$GMAGICK_VERSION.tgz
-    mv gmagick-$GMAGICK_VERSION gmagick-$GMAGICK_VERSION-$PHP_VERSION
-    cd gmagick-$GMAGICK_VERSION-$PHP_VERSION
+    mv gmagick-$GMAGICK_VERSION gmagick-$GMAGICK_VERSION-$PHP_VERSION-$GRAPHICSMAGIC_VERSION
+    cd gmagick-$GMAGICK_VERSION-$PHP_VERSION-$GRAPHICSMAGIC_VERSION
     phpize
     ./configure --with-gmagick=/opt/gmagick
     make -j
 else
-    cd gmagick-$GMAGICK_VERSION-$PHP_VERSION
+    cd gmagick-$GMAGICK_VERSION-$PHP_VERSION-$GRAPHICSMAGIC_VERSION
 fi
 
 sudo make install
-echo "extension=gmagick.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo 'extension=gmagick.so' >> `php --ini | grep 'Loaded Configuration' | sed -e 's|.*:\s*||'`
 php --ri gmagick
--- .travis/imagick.sh
+++ .travis/imagick.sh
@@ -2,15 +2,16 @@
 
 set -xe
 
-IMAGEMAGICK_VERSION="6.8.9-10"
-IMAGICK_VERSION="3.4.3"
+IMAGEMAGICK_VERSION='6.8.9-10'
+IMAGICK_VERSION='3.4.3'
+
 PHP_VERSION=`php -r 'echo PHP_VERSION_ID;'`
+IMAGEMAGICK_MAJORVERSION=`printf %s "$IMAGEMAGICK_VERSION" | cut -f1 -d'.'`
 
 mkdir -p cache
 cd cache
 
-if [ ! -e ./ImageMagick-$IMAGEMAGICK_VERSION ]
-then
+if [ ! -e ./ImageMagick-$IMAGEMAGICK_VERSION ]; then
     rm -rf ./ImageMagick-* || true
     wget http://www.imagemagick.org/download/releases/ImageMagick-$IMAGEMAGICK_VERSION.tar.xz
     tar -xf ImageMagick-$IMAGEMAGICK_VERSION.tar.xz
@@ -24,24 +25,26 @@ fi
 
 sudo make install
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/imagemagick/lib/pkgconfig
-sudo ln -s /opt/imagemagick/include/ImageMagick-6 /opt/imagemagick/include/ImageMagick
+if [ -L /opt/imagemagick/include/ImageMagick ]; then
+    unlink /opt/imagemagick/include/ImageMagick
+fi 
+sudo ln -s /opt/imagemagick/include/ImageMagick-$IMAGEMAGICK_MAJORVERSION /opt/imagemagick/include/ImageMagick
 cd ..
 
-if [ ! -e ./imagick-$IMAGICK_VERSION-$PHP_VERSION ]
-then
+if [ ! -e ./imagick-$IMAGICK_VERSION-$PHP_VERSION-$IMAGEMAGICK_VERSION ]; then
     rm -rf ./imagick-* || true
     wget https://pecl.php.net/get/imagick-$IMAGICK_VERSION.tgz
     tar -xzf imagick-$IMAGICK_VERSION.tgz
     rm imagick-$IMAGICK_VERSION.tgz
-    mv imagick-$IMAGICK_VERSION imagick-$IMAGICK_VERSION-$PHP_VERSION
-    cd imagick-$IMAGICK_VERSION-$PHP_VERSION
+    mv imagick-$IMAGICK_VERSION imagick-$IMAGICK_VERSION-$PHP_VERSION-$IMAGEMAGICK_VERSION
+    cd imagick-$IMAGICK_VERSION-$PHP_VERSION-$IMAGEMAGICK_VERSION
     phpize
     ./configure --with-imagick=/opt/imagemagick
     make -j
 else
-    cd imagick-$IMAGICK_VERSION-$PHP_VERSION
+    cd imagick-$IMAGICK_VERSION-$PHP_VERSION-$IMAGEMAGICK_VERSION
 fi
 
 sudo make install
-echo "extension=imagick.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo 'extension=imagick.so' >> `php --ini | grep 'Loaded Configuration' | sed -e 's|.*:\s*||'`
 php --ri imagick
