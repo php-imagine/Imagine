96ab6314e6aa73d7a0f1a64728cc1046ab378202
marek <m.necesany@choosit.com>
2019-05-28 11:29:48 +0200

    feature - more information in Imagine RuntimeException

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -11,9 +11,21 @@
 
 namespace Imagine\Exception;
 
+use Throwable;
+
 /**
  * Imagine-specific runtime exception.
  */
 class RuntimeException extends \RuntimeException implements Exception
 {
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        $lastError = error_get_last();
+        $message = isset($lastError['message'])
+            ? sprintf('%s (last PHP runtime error message: %s)', $message, $lastError['message'])
+            : $message
+        ;
+
+        parent::__construct($message, $code, $previous);
+    }
 }
