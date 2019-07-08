56a0a4b379ac49ca88b4328feca40705dd341a59
Michele Locati <michele@locati.it>
2019-07-08 19:05:56 +0200

    Use webp_quality parameter only if it's set

--- src/Gd/Image.php
+++ src/Gd/Image.php
@@ -663,8 +663,8 @@ final class Image extends AbstractImage
                     if ($options['webp_quality'] < 0 || $options['webp_quality'] > 100) {
                         throw new InvalidArgumentException('webp_quality option should be an integer from 0 to 100');
                     }
+                    $args[] = $options['webp_quality'];
                 }
-                $args[] = $options['webp_quality'];
                 break;
         }
 
