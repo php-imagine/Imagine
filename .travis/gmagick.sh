a20f0e02380c8aad33ae4771097e4f78d95742cd
Martin Auswöger <martin@auswoeger.com>
2020-11-03 21:14:54 +0100

    (Try to) Fix segmentation fault

--- .travis/gmagick.sh
+++ .travis/gmagick.sh
@@ -20,7 +20,7 @@ cd cache
 if [ "$GRAPHICSMAGIC_VERSION" = 'ondrej' ]; then
     sudo add-apt-repository -y ppa:ondrej/php
     sudo apt-get -q update
-    sudo apt-get -y install graphicsmagick graphicsmagick-dbg libgraphicsmagick1-dev php7.3
+    sudo apt-get -y install graphicsmagick graphicsmagick-dbg libgraphicsmagick1-dev php7.3 libxml2
 else
     if [ ! -e ./GraphicsMagick-$GRAPHICSMAGIC_VERSION ]; then
         rm -rf ./GraphicsMagick-* || true
