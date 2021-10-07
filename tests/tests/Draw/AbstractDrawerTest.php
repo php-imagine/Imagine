b34168e8826195ab6b88763fda70be2136320bb1
Michele Locati <michele@locati.it>
2021-10-07 10:48:03 +0200

    Let DrawerTest::testArc be less strict

--- tests/tests/Draw/AbstractDrawerTest.php
+++ tests/tests/Draw/AbstractDrawerTest.php
@@ -54,7 +54,7 @@ abstract class AbstractDrawerTest extends ImagineTestCase
         $this->assertSame($drawer, $drawer->arc(new Center($size), $size->scale(0.5), 0, 180, $this->getColor('f00')));
         $filename = $this->getTemporaryFilename("thinkness{$thickness}.png");
         $image->save($filename);
-        $this->assertImageEquals(IMAGINE_TEST_FIXTURESFOLDER . "/drawer/arc/thinkness{$thickness}.png", $filename, '', 0.25, $imagine);
+        $this->assertImageEquals(IMAGINE_TEST_FIXTURESFOLDER . "/drawer/arc/thinkness{$thickness}.png", $filename, '', 0.45, $imagine);
     }
 
     /**
