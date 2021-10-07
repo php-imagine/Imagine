030854aac4633f4c6a6bfb3a0b8930c016f87bd5
Michele Locati <michele@locati.it>
2021-10-07 11:01:06 +0200

    Let DrawerTest::testText be less strict

--- tests/tests/Draw/AbstractDrawerTest.php
+++ tests/tests/Draw/AbstractDrawerTest.php
@@ -225,7 +225,7 @@ abstract class AbstractDrawerTest extends ImagineTestCase
         ));
         $filename = $this->getTemporaryFilename('.png');
         $image->save($filename);
-        $this->assertImageEquals(IMAGINE_TEST_FIXTURESFOLDER . '/drawer/text/text.png', $filename, '', 0.18, $imagine);
+        $this->assertImageEquals(IMAGINE_TEST_FIXTURESFOLDER . '/drawer/text/text.png', $filename, '', 0.9, $imagine);
     }
 
     public function testDrawASmileyFace()
